860ebd310f5ddfa0d3ea5e5d2d498d8f8892014d
Aashis <aashiislama@gmail.com>
2020-06-22 23:11:13 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>This is index.php title1</title>
+		<link rel="stylesheet" href="css/index.css" />
+		<script src="js/index.js"></script>
+		<meta charset="utf-8">
+
+	</head>
+	<body>
+
+	</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,1 @@
+window.alert("hello this is index.js");
