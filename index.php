b608b26aed6e7014dfacac4bdf046264df77a011
Aashis <aashiislama@gmail.com>
2020-06-22 23:33:18 +0530

    commit 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
-		<title>This is index.php title1</title>
+		<title>This is index.php title commit 3</title>
 		<link rel="stylesheet" href="css/index.css" />
 		<script src="js/index.js"></script>
 		<meta charset="utf-8">
@@ -9,5 +9,6 @@
 	</head>
 	<body>
 		<h1>Added heading for my website</h1>
+		<button id="btn1">Hey click on me !</button>
 	</body>	
 </html>
